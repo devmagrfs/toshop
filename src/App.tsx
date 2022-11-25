0c9598028a04fb39f70e23a2a4d8197fbf2bb37d
devmagrfs <devmagrfs@gmail.com>
2022-11-25 17:23:18 +0900

    [Feat] 라우터 설정 및 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import NotFound from './pages/NotFound';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import './App.css';
+import AllProducts from './pages/AllProducts';
+import ProductDetail from './pages/ProductDetail';
+import NewProduct from './pages/NewProduct';
+import Cart from './pages/Cart';
+import Favorite from './pages/Favorite';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +20,14 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <Layout />,
 		errorElement: <NotFound />,
-		children: [{ index: true, element: <Home /> }],
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'products', element: <AllProducts /> },
+			{ path: 'products/:id', element: <ProductDetail /> },
+			{ path: 'products/new', element: <NewProduct /> },
+			{ path: 'cart', element: <Cart /> },
+			{ path: 'favorite', element: <Favorite /> },
+		],
 	},
 ]);
 
