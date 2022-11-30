8b0ba667c3dc9c49a010b941ee37c927f1703f14
devmagrfs <devmagrfs@gmail.com>
2022-11-30 10:33:51 +0900

    [Feat] 헤더에 페이지 이동  기능 추가 및 기본 스타일링 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineShoppingCart, AiOutlineLogin, AiOutlineLogout, AiOutlineMenu } from 'react-icons/ai';
+import { BsPencilSquare } from 'react-icons/bs';
 import { ReactComponent as Logo } from '../assets/ToShopLogo.svg';
 
 const Header = () => {
 	return (
 		<header className='flex justify-between items-center w-full border-b border-zinc-600 mb-4 p-4'>
-			<Link to='/' className='flex items-center'>
-				<Logo className='font-bold' />
-				<h1 className='text-3xl font-bold ml-2'>ToShop</h1>
+			<Link to='/' className='flex items-center text-4xl text-brand font-bold'>
+				<Logo />
+				<h1 className='ml-2'>ToShop</h1>
 			</Link>
-			<nav className='flex justify-end items-center text-2xl font-bold w-full'>
-				<span className='cursor-pointer mr-3'>Menu</span>
-				<span className='cursor-pointer mr-3'>즐겨찾기</span>
-				<AiOutlineShoppingCart className='cursor-pointer mr-3' />
+			<nav className='flex justify-end items-center text-2xl font-semibold w-full gap-3'>
+				<span className='cursor-pointer'>Menu</span>
+				<Link to='/favorite'>
+					<span>즐겨찾기</span>
+				</Link>
+				<Link to='/products/new'>
+					<BsPencilSquare />
+				</Link>
+				<Link to='/cart'>
+					<AiOutlineShoppingCart />
+				</Link>
 				<AiOutlineLogin className='cursor-pointer' />
 			</nav>
 		</header>
