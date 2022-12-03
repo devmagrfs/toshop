1cbc9f87d4eb248605e25ddb96b9ac40a7858775
devmagrfs <devmagrfs@gmail.com>
2022-12-04 03:00:35 +0900

    [Feat] ui 컴포넌트인 Button 컴포넌트 생성

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProp {
+	text: string;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({ text, onClick }: ButtonProp) => {
+	return (
+		<button className='bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110' onClick={onClick}>
+			{text}
+		</button>
+	);
+};
+
+export default Button;
