54e7bc4856c5f33a45fe081f4ecced49c8c21cb2
devmagrfs <devmagrfs@gmail.com>
2022-12-13 00:08:15 +0900

    [Feat] 업로드 페이지에 cloudinary 업로드 기능 이어주기

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
 
 interface ProductProp {
@@ -12,9 +13,14 @@ interface ProductProp {
 const NewProduct = () => {
 	const [product, setProduct] = useState<ProductProp | undefined>(undefined);
 	const [file, setFile] = useState<File | null>();
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		uploadImage(file).then((url) => {
+			console.log(url);
+		});
 	};
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value, files } = e.target;
 		if (name === 'file') {
@@ -34,8 +40,8 @@ const NewProduct = () => {
 				<input type='text' name='category' value={product?.category ?? ''} placeholder='카테고리' required onChange={handleChange} />
 				<input type='text' name='describe' value={product?.describe ?? ''} placeholder='제품 설명' required onChange={handleChange} />
 				<input type='text' name='options' value={product?.options ?? ''} placeholder='옵션들(콤마(,)로 구분하기)' required onChange={handleChange} />
+				<Button text={'제품 등록'} />
 			</form>
-			<Button text={'제품 등록'} />
 		</section>
 	);
 };
