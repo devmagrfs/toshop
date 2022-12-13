d4cc479b91bcacfb2512312bb0daeec767048a18
devmagrfs <devmagrfs@gmail.com>
2022-12-13 16:59:44 +0900

    [Feat] disabled에 대한 대응 추가

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 interface ButtonProp {
 	text: string;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	disabled?: boolean;
 }
 
-const Button = ({ text, onClick }: ButtonProp) => {
+const Button = ({ text, onClick, disabled }: ButtonProp) => {
 	return (
-		<button className='bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110' onClick={onClick}>
+		<button className='bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110' onClick={onClick} disabled={disabled}>
 			{text}
 		</button>
 	);
